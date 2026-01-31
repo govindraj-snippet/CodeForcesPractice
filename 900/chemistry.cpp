68c6456f5ea22ac8f70e135a1bb8bcbe04ad8b06
govindraj-snippet <2023bit035@sggs.ac.in>
2026-01-31 17:48:46 +0530

    solved 2 questions

--- 900/chemistry.cpp
+++ 900/chemistry.cpp
@@ -5,18 +5,33 @@ int main(){
 
     int t ; 
     cin >> t ; 
+
     while( t-- ){
         int n ; 
-        int k ; 
         cin >> n ; 
+
+        int k ; 
         cin >> k ; 
+
         string s ; 
         cin >> s ; 
+
         unordered_map< char , int > mpp ; 
-        for( int i =0 ; i < s.length() ; i++ ){
-            mpp[s[i]]++ ; 
+        for( int i = 0 ; i < n ; i++ ){
+            mpp[ s[i] ]++ ; 
+        }
+        int odds = 0 ; 
+
+        for( auto it : mpp ){
+            if( it.second % 2 != 0 ){
+                odds++ ; 
+            }
         }
 
-
+        if( odds > k + 1   ) cout << "NO" << endl ; 
+        else cout << "YES" << endl ; 
     }
+
+    return 0 ; 
+
 }

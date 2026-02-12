46e2bbcdde386374d75b591c100a748374ff3cad
govindraj-snippet <2023bit035@sggs.ac.in>
2026-02-12 11:56:05 +0530

    done

--- 800/oneAndtwo.cpp
+++ 800/oneAndtwo.cpp
@@ -6,6 +6,26 @@ int main(){
     cin >> t ; 
 
     while(t--){
-        
+        int n ; 
+        cin >> n ; 
+        vector<long long >arr( n );
+        for(int i = 0 ; i < n ;i++  ){
+            cin >> arr[i] ; 
+        }
+        long long  totalMult = 1 ; 
+        for( auto it : arr ){
+            totalMult *= it ; 
+        }
+        long long prefixMult = arr[0] ; 
+        long long  k = -1 ; 
+        for(int i = 1 ; i < n ; i++ ){
+            prefixMult *= arr[i] ; 
+            if( prefixMult == totalMult/prefixMult){
+                k = i ; 
+                break ; 
+            }
+        }
+        cout << k << endl ; 
     }
+    return 0 ; 
 }

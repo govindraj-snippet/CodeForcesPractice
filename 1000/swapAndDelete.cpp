221789aa225f290985ace36de8f1129a58e886b4
govindraj-snippet <2023bit035@sggs.ac.in>
2026-02-14 08:22:13 +0530

    swap and Delete

--- 1000/swapAndDelete.cpp
+++ 1000/swapAndDelete.cpp
@@ -7,6 +7,7 @@ int main(){
     while(t--){
         string s ; 
         cin >> s ; 
+        int n = s.length() ; 
         int one = 0  ;
         int zero = 0 ; 
         for( auto it : s ){
@@ -14,6 +15,20 @@ int main(){
             else one++ ; 
         }
        // cout << abs( zero - one ) << endl ; 
+       int len = 0 ; 
+       for(int i = 0 ; i < n ; i++ ){
+        if( s[i] == '0'&& one > 0 ){
+            one-- ; 
+            len++ ; 
+        }
+        else if(s[i] == '1'&& zero > 0  ){
+            zero-- ; 
+            len++ ; 
+        }
+        else break ; 
+       }
+       cout << n - len << endl ; 
+
     }
     return 0 ; 
 }

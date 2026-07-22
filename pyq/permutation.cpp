2f75b9e88538bae9086c48ac515eafead4524227
govindraj-snippet <2023bit035@sggs.ac.in>
2026-07-22 21:02:51 +0530

    done

---
+++ pyq/permutation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <utility>
+#include <iomanip>
+using namespace std ; 
+
+int main(){
+    string s ;
+    getline(cin , s) ;
+    stringstream ss(s) ; 
+    
+
+}

6a7a77f48be1b3b8bbd1509fa7d9b2c330d2dd5c
govindraj-snippet <2023bit035@sggs.ac.in>
2026-09-04 12:58:19 +0530

    odd.cpp

---
+++ 800/badboy.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main(){
+    int t; 
+    cin >> t; 
+    while(t--){
+        long long n , m , x , y ;
+        cin >> n ; 
+        cin >> m ; 
+        cin >> x ; 
+        cin >> y ;
+        cout << 1 << " " << 1 << " " << n << " " << m << endl ;
+        return 0; 
+
+    }
+}
